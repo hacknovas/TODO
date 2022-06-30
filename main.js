b92f8c16e44a156a228ea5dc2418cf19c1ccb3e9
HackNovas <creatives.doni@gmail.com>
2022-06-30 23:51:59 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,133 @@
+const form = document.querySelector("#itemForm");
+const itemInput = document.querySelector("#itemInput");
+const itemList = document.querySelector("#itemList");
+const messageDiv = document.querySelector("#message");
+const clearButton = document.querySelector("#clearBtn");
+const filters = document.querySelectorAll(".nav-item");
+
+const handleItem = function (itemData) {
+  const items = document.querySelectorAll(".list-group-item");
+  items.forEach((item) => {
+    if (
+      item.querySelector(".title").getAttribute("data-time") == itemData.addedAt
+    ) {
+      item
+        .querySelector("[data-delete]")
+        .addEventListener("click", function (e) {
+          e.preventDefault();
+          itemList.removeChild(item);
+          removeItem(item);
+          setLocalStorage(todoItems);
+          return todoItems.filter((item) => item != itemData);
+        });
+    }
+  });
+};
+
+let todoItems = [];
+
+const getItemsFilter = function (type) {
+  let filterItems = [];
+  console.log(type);
+  switch (type) {
+    case "todo":
+      filterItems = todoItems.filter((item) => !item.isDone);
+      break;
+    case "done":
+      filterItems = todoItems.filter((item) => item.isDone);
+      break;
+    default:
+      filterItems = todoItems;
+  }
+  getList(filterItems);
+};
+
+const removeItem = function (item) {
+  const removeIndex = todoItems.indexOf(item);
+  todoItems.splice(removeIndex, 1);
+};
+
+
+const getList = function (todoItems) {
+  itemList.innerHTML = "";
+  if (todoItems.length > 0) {
+    todoItems.forEach((item) => {
+      const iconClass = item.isDone;
+      itemList.insertAdjacentHTML(
+        "beforeend",
+        `<li class="list-group-item d-flex justify-content-between align-items-center">
+          <span class="title" data-time="${item.addedAt}">${item.name}</span> 
+          <span>
+              <a href="#" data-done><i class="bi ${iconClass} green"></i></a>
+              <a href="#" data-delete><i class="bi bi-x-circle red"></i></a>
+          </span>
+        </li>`
+      );
+      handleItem(item);
+    });
+  } else {
+    itemList.insertAdjacentHTML(
+      "beforeend",
+      `<li class="list-group-item d-flex justify-content-between align-items-center">
+        No record found.
+      </li>`
+    );
+  }
+};
+
+const getLocalStorage = function () {
+  const todoStorage = localStorage.getItem("todoItems");
+  if (todoStorage == "undefined" || todoStorage == null) {
+    todoItems = [];
+  } else {
+    todoItems = JSON.parse(todoStorage);
+  }
+  getList(todoItems);
+};
+
+const setLocalStorage = function (todoItems) {
+  localStorage.setItem("todoItems", JSON.stringify(todoItems));
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const itemName = itemInput.value.trim();
+    if (itemName.length == 0) {
+      return;
+    } else {
+      const currenItemIndex = document.querySelector("#citem").value;
+      if (currenItemIndex) {
+        updateItem(currenItemIndex, itemName);
+        document.querySelector("#citem").value = "";
+      } else {
+        const itemObj = {
+          name: itemName,
+          isDone: false,
+          addedAt: new Date().getTime(),
+        };
+        todoItems.push(itemObj);
+        setLocalStorage(todoItems);
+      }
+
+      getList(todoItems);
+    }
+    console.log(todoItems);
+    itemInput.value = "";
+  });
+
+  filters.forEach((tab) => {
+    tab.addEventListener("click", function (e) {
+      e.preventDefault();
+      const tabType = this.getAttribute("data-type");
+      document.querySelectorAll(".nav-link").forEach((nav) => {
+        nav.classList.remove("active");
+      });
+      this.firstElementChild.classList.add("active");
+      document.querySelector("#filterType").value = tabType;
+      getItemsFilter(tabType);
+    });
+  });
+
+  getLocalStorage();
+});
